4774f85466d85f8864202ab756a1792cfd5e7b3c
garypwn <garypwn@users.noreply.github.com>
2018-06-19 22:10:22 -0700

    Refactor class and interface names
    Rename several types and methods to better reflect their function.
     - Channel has been renamed to MessagePipe.
    	- the term "member" has been replaced with "viewer"
     - ChannelController has been renamed to Channel
     - Controllers has been renamed to ChannelController

--- src/main/java/net/comecraft/comechat/channel/Channel.java
+++ src/main/java/net/comecraft/comechat/channel/Channel.java
@@ -1,50 +1,220 @@
 package net.comecraft.comechat.channel;
 
+import java.util.Set;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.permissions.Permission;
 
 import com.google.common.collect.ImmutableSet;
 
 import net.comecraft.comechat.format.ChatFormatTemplate;
 
 /**
- * Represents a stream of Messages that can be read or posted to.
+ * Channel represents a set of message pipes that can be reached by a
+ * command or alias. When a user tries to speak in a specified channel, for
+ * example by using /g for global chat or /f for faction chat, the
+ * Channel checks their permissions, location, faction, world, etc.
+ * and sends them to the correct pipe. <br>
+ * <br>
+ * The Channel should listen for events that would result in a channel
+ * change. For example, a world-only chat should listen for a world change, and
+ * a faction chat should listen for a faction change.
  */
-public interface Channel {
+public abstract class Channel implements CommandExecutor, Listener {
 
 	/**
-	 * Gets a set containing all CommandSenders who will be shown messages
-	 * from this channel. The set should be immutable. If you want to add or
-	 * remove members from this channel use addMember() or removeMember() respectively.
-	 * @return A set containing the members of this Channel.
+	 * Gets the unique identifier for this channel.
+	 * 
+	 * @return The unique ID for this channel.
 	 */
-	public ImmutableSet<CommandSender> getMembers();
-	
+	public abstract String getId();
+
 	/**
-	 * Removes a member from the channel if it is present.
-	 * @param member The member to remove from the Channel
-	 * @return <tt>true</tt> if successful.
+	 * Gets the aliases for this channel. Aliases are usually one or two characters
+	 * long and abbreviate the channel's ID. If you need to modify a
+	 * Channel's aliases, do so through Config, so that listeners and
+	 * command aliases can be updated.
+	 * 
+	 * @return A set containing the aliases for this Channel.
 	 */
-	public boolean removeMember(CommandSender member);
-	
+	public abstract ImmutableSet<String> getAliases();
+
 	/**
-	 * Adds a member to the channel.
-	 * @param member The member to add.
-	 * @return <tt>true</tt> if the member was not already present.
+	 * Gets the pipes controlled by this Channel.
+	 * 
+	 * @return The set of Channels that is controlled by this Channel.
 	 */
-	public boolean addMember(CommandSender member);
-	
+	public abstract ImmutableSet<MessagePipe> getChannels();
+
 	/**
-	 * Sends a message to all members of the channel.
-	 * Should pipe the message text to all chat formatters and send the message
-	 * to each receiver.
-	 * @param sender The sender of the message.
-	 * @param message The message to send.
+	 * Get the format template for this Channel
+	 * 
+	 * @return The ChatFormatTemplate used by this Channel.
 	 */
-	public void sendMessage(CommandSender sender, String message);
-	
+	public abstract ChatFormatTemplate getTemplate();
+
 	/**
-	 * Get the formatting template used to format messages in this Channel.
-	 * @return The ChatFormat used by this Channel.
+	 * Gets the permission required to read messages in this channel.
+	 * 
+	 * @return The Permission required to read messages in this channel.
 	 */
-	public ChatFormatTemplate getFormat();
+	public abstract Permission getReadPerm();
+
+	/**
+	 * Gets the permission required to write messages in to channel.
+	 * 
+	 * @return The Permission required to write messages in to channel.
+	 */
+	public abstract Permission getWritePerm();
+
+	/**
+	 * Prevents a sender from sending messages to this channel. Silencing is
+	 * different from lack of write permission.
+	 * 
+	 * @param mutee
+	 *            The sender to silence.
+	 * @return <tt>true</tt> if the sender was not already silenced.
+	 */
+	public abstract boolean silence(CommandSender mutee);
+
+	/**
+	 * Unsilences a sender. Silencing is different from lack of write permission.
+	 * 
+	 * @param mutee
+	 *            The sender to unsilence.
+	 * @return <tt>true</tt> if the sender was previously silenced.
+	 */
+	public abstract boolean unSilence(CommandSender mutee);
+
+	/**
+	 * Checks whether a sender is silenced. Silencing is different from lack of
+	 * write permission.
+	 * 
+	 * @param mutee
+	 *            The sender to check.
+	 * @return <tt>true</tt> if the sender is silenced.
+	 */
+	public abstract boolean isSilenced(CommandSender mutee);
+
+	/**
+	 * Gets the senders who are silenced from this channel. Silencing is different
+	 * from lack of write permission.
+	 * 
+	 * @return A set containing this channel's silenced senders.
+	 */
+	public abstract Set<CommandSender> getSilenced();
+
+	/**
+	 * Prevents a sender from viewing messages sent to this channel. Deafening is
+	 * different from lack of read permission.
+	 * 
+	 * @param muter
+	 *            The sender to deafen.
+	 * @return <tt>true</tt> if the sender was not already deafened.
+	 */
+	public abstract boolean deafen(CommandSender muter);
+
+	/**
+	 * Undeafens a sender. Deafening is different from lack of read permission.
+	 * 
+	 * @param muter
+	 *            The sender to undeafen.
+	 * @return <tt>true</tt> if the sender was previously deafened.
+	 */
+	public abstract boolean unDeafen(CommandSender muter);
+
+	/**
+	 * Checks whether a sender is deafened. Deafening is different from lack of read
+	 * permission.
+	 * 
+	 * @param mutee
+	 *            The sender to check.
+	 * @return <tt>true</tt> if the sender is deafened.
+	 */
+	public abstract boolean isDeafened(CommandSender muter);
+
+	/**
+	 * Gets the senders who are deafened from this channel. Deafening is different
+	 * from lack of read permission.
+	 * 
+	 * @return A set containing this channel's deafened senders.
+	 */
+	public abstract Set<CommandSender> getDeafened();
+
+	/**
+	 * Sends a message to the appropriate channel.
+	 * 
+	 * @param sender
+	 *            The sender of the message.
+	 * @param message
+	 *            The raw input text of the message.
+	 */
+	public abstract void sendMessage(CommandSender sender, String message);
+
+	/**
+ 	 * If no chat message is included in the arguments, try to change
+	 * this to the sender's active channel. If a chat message is included in the
+	 * arguments, try to send that message.
+	 */
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		// TODO Check sender perms
+
+		// TODO If the sender has this channel deafened, undeafen it.
+		// TODO If the sender is silenced from this channel, notify them.
+
+		// Channel change
+		if (args.length == 0) {
+			// TODO Change active channel of sender
+			// TODO Notify sender that they have changed channels
+			// TODO Add channel change message to lang file
+			// TODO Fire channel change event
+			return true;
+		}
+
+		// Send the message.
+		sendMessage(sender, String.join(" ", args));
+		return true;
+	}
+
+	/**
+	 * On player join, put that player into the correct pipe.
+	 * 
+	 * @param event
+	 *            The PlayerJoinEvent fired by the player joining the server.
+	 */
+	@EventHandler
+	public abstract void onPlayerJoin(PlayerJoinEvent event);
+
+	/**
+	 * On player leave, remove that player from any pipes.
+	 * 
+	 * @param event
+	 *            The PlayerQuitEvent fired by the player leaving the server.
+	 */
+	@EventHandler
+	public abstract void onPlayerQuit(PlayerQuitEvent event);
+
+	@Override
+	public int hashCode() {
+		return getId().hashCode();
+	}
+
+	/**
+	 * Only checks that the IDs of the two objects are equal.
+	 * 
+	 * @inheritDoc
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		return getId().equals(((ChatFormatTemplate) obj).getId());
+	}
 }
--- src/main/java/net/comecraft/comechat/channel/ChannelController.java
+++ src/main/java/net/comecraft/comechat/channel/ChannelController.java
@@ -1,220 +1,84 @@
 package net.comecraft.comechat.channel;
 
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.permissions.Permission;
 
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableSet;
 
-import net.comecraft.comechat.format.ChatFormatTemplate;
-
 /**
- * ChannelController represents a set of channels that can be reached by a
- * command or alias. When a user tries to speak in a specified channel, for
- * example by using /g for global chat or /f for faction chat, the
- * ChannelController checks their permissions, location, faction, world, etc.
- * and sends them to the correct Channel. <br>
- * <br>
- * The ChannelController should listen for events that would result in a channel
- * change. For example, a world-only chat should listen for a world change, and
- * a faction chat should listen for a faction change.
+ * Represents the collection of all channels. Keeps track
+ * of senders' active channel. Manages channel aliases and makes sure
+ * there are no conflicts.
  */
-public abstract class ChannelController implements CommandExecutor, Listener {
-
+public final class ChannelController {
+	
+	private final Map<Command, Channel> channels;
+	private final Map<CommandSender, Channel> activeChannels;
+	private static ChannelController channelController;
+	
+	public ChannelController() {
+		channels = new HashMap<Command, Channel>();
+		activeChannels = new HashMap<CommandSender, Channel>();
+		channelController = this;
+		// TODO get the config and add controllers from config
+	}
+	
 	/**
-	 * Gets the unique identifier for this channel controller.
-	 * 
-	 * @return The unique ID for this channel.
+	 * Gets the current channelcontroller instance.
+	 * @return The current ChannelController instance.
 	 */
-	public abstract String getId();
-
+	public static final ChannelController get() {
+		return channelController;
+	}
+	
 	/**
-	 * Gets the aliases for this channel. Aliases are usually one or two characters
-	 * long and abbreviate the channel controller's ID. If you need to modify a
-	 * ChannelController's aliases, do so through Config, so that listeners and
-	 * command aliases can be updated.
-	 * 
-	 * @return A set containing the aliases for this ChannelController.
+	 * Gets the set of channels.
+	 * @return A set containing all registered channels.
 	 */
-	public abstract ImmutableSet<String> getAliases();
-
+	public ImmutableSet<Channel> getControllers() {
+		return ImmutableSet.copyOf(channels.values());
+	}
+	
 	/**
-	 * Gets the channels controlled by this ChannelController.
-	 * 
-	 * @return The set of Channels that is controlled by this ChannelController.
+	 * Register a new Channel in the master list.
+	 * @param channel The channel to be registered.
+	 * @param command The command to register the channel for.
+	 * @return <tt>false</tt> if this command already mapped to a channel.
+	 * @throws IllegalArgumentException If adding the channel would cause an alias conflict.
 	 */
-	public abstract ImmutableSet<Channel> getChannels();
-
+	public Optional<Channel> registerChannel(Command command, Channel channel) throws IllegalArgumentException {
+		// TODO check for alias conflicts
+		/* TODO
+		 * Register new aliases as commands, remove old aliases.
+		 * This will probably require a server restart, modifying the
+		 * plugin.yml, or using reflection to obtain the server's commandmap.
+		 */
+		return null;
+	}
+	
 	/**
-	 * Get the format template for this ChannelController
-	 * 
-	 * @return The ChatFormatTemplate used by this ChannelController.
+	 * Set a sender's active channel. Chat inputs that don't specify channel will go to this channel.
+	 * @param sender The sender to change the active channel for.
+	 * @param channel The new active channel.
+	 * @return <tt>false</tt> if the sender's active channel did not change.
 	 */
-	public abstract ChatFormatTemplate getTemplate();
-
-	/**
-	 * Gets the permission required to read messages in this channel.
-	 * 
-	 * @return The Permission required to read messages in this channel.
-	 */
-	public abstract Permission getReadPerm();
-
-	/**
-	 * Gets the permission required to write messages in to channel.
-	 * 
-	 * @return The Permission required to write messages in to channel.
-	 */
-	public abstract Permission getWritePerm();
-
-	/**
-	 * Prevents a sender from sending messages to this controller. Silencing is
-	 * different from lack of write permission.
-	 * 
-	 * @param mutee
-	 *            The sender to silence.
-	 * @return <tt>true</tt> if the sender was not already silenced.
-	 */
-	public abstract boolean silence(CommandSender mutee);
-
-	/**
-	 * Unsilences a sender. Silencing is different from lack of write permission.
-	 * 
-	 * @param mutee
-	 *            The sender to unsilence.
-	 * @return <tt>true</tt> if the sender was previously silenced.
-	 */
-	public abstract boolean unSilence(CommandSender mutee);
-
-	/**
-	 * Checks whether a sender is silenced. Silencing is different from lack of
-	 * write permission.
-	 * 
-	 * @param mutee
-	 *            The sender to check.
-	 * @return <tt>true</tt> if the sender is silenced.
-	 */
-	public abstract boolean isSilenced(CommandSender mutee);
-
-	/**
-	 * Gets the senders who are silenced from this channel. Silencing is different
-	 * from lack of write permission.
-	 * 
-	 * @return A set containing this controller's silenced senders.
-	 */
-	public abstract Set<CommandSender> getSilenced();
-
-	/**
-	 * Prevents a sender from viewing messages sent to this controller. Deafening is
-	 * different from lack of read permission.
-	 * 
-	 * @param muter
-	 *            The sender to deafen.
-	 * @return <tt>true</tt> if the sender was not already deafened.
-	 */
-	public abstract boolean deafen(CommandSender muter);
-
-	/**
-	 * Undeafens a sender. Deafening is different from lack of read permission.
-	 * 
-	 * @param muter
-	 *            The sender to undeafen.
-	 * @return <tt>true</tt> if the sender was previously deafened.
-	 */
-	public abstract boolean unDeafen(CommandSender muter);
-
-	/**
-	 * Checks whether a sender is deafened. Deafening is different from lack of read
-	 * permission.
-	 * 
-	 * @param mutee
-	 *            The sender to check.
-	 * @return <tt>true</tt> if the sender is deafened.
-	 */
-	public abstract boolean isDeafened(CommandSender muter);
-
-	/**
-	 * Gets the senders who are deafened from this channel. Deafening is different
-	 * from lack of read permission.
-	 * 
-	 * @return A set containing this controller's deafened senders.
-	 */
-	public abstract Set<CommandSender> getDeafened();
-
-	/**
-	 * Sends a message to the appropriate channel.
-	 * 
-	 * @param sender
-	 *            The sender of the message.
-	 * @param message
-	 *            The raw input text of the message.
-	 */
-	public abstract void sendMessage(CommandSender sender, String message);
-
-	/**
- 	 * If no chat message is included in the arguments, try to change
-	 * this to the sender's active channel. If a chat message is included in the
-	 * arguments, try to send that message.
-	 */
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		// TODO Check sender perms
-
-		// TODO If the sender has this channel deafened, undeafen it.
-		// TODO If the sender is silenced from this channel, notify them.
-
-		// Channel change
-		if (args.length == 0) {
-			// TODO Change active channel of sender
-			// TODO Notify sender that they have changed channels
-			// TODO Add channel change message to lang file
-			// TODO Fire channel change event
-			return true;
-		}
-
-		// Send the message.
-		sendMessage(sender, String.join(" ", args));
+	public boolean setActiveChannel(CommandSender sender, Channel channel) {
+		// TODO
 		return true;
 	}
-
+	
 	/**
-	 * On player join, put that player into the correct channel.
-	 * 
-	 * @param event
-	 *            The PlayerJoinEvent fired by the player joining the server.
+	 * Gets a sender's active channel.
+	 * @param sender The sender to get the active channel for.
+	 * @return The sender's active channel.
 	 */
-	@EventHandler
-	public abstract void onPlayerJoin(PlayerJoinEvent event);
-
-	/**
-	 * On player leave, remove that player from any channels.
-	 * 
-	 * @param event
-	 *            The PlayerQuitEvent fired by the player leaving the server.
-	 */
-	@EventHandler
-	public abstract void onPlayerQuit(PlayerQuitEvent event);
-
-	@Override
-	public int hashCode() {
-		return getId().hashCode();
+	public Channel getActiveChannel(CommandSender sender) {
+		return activeChannels.get(sender);
 	}
-
-	/**
-	 * Only checks that the IDs of the two objects are equal.
-	 * 
-	 * @inheritDoc
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null)
-			return false;
-		return getId().equals(((ChatFormatTemplate) obj).getId());
-	}
+	
+	
 }
